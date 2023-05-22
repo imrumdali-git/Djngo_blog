47f0e16bbc5216adb1d672ba588a091ee8d576f2
Anil Rumdali Rai <anil.rai704757@gmail.com>
2023-05-22 23:01:55 +0545

    6th change, newblog

--- ablog/urls.py
+++ ablog/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('theblog.urls')),
+    path('member/', include('django.contrib.auth.urls')),
+    path('member/', include('members.urls')),
 ]
